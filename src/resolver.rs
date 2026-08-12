f4ffa706357f0a603c06800eeca1e6d22a884c7b
WindSoilder <WindSoilder@outlook.com>
2026-08-12 21:44:45 +0800

    resolve block, introduce scopes into Resolver

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,6 +1,13 @@
-struct Resolver;
 use crate::{Block, Expr, Stmt};
 use anyhow::Result;
+use std::collections::HashMap;
+
+struct Resolver {
+    /// This field keep track of the stack of scopes currently, in scope.
+    /// Each element is a HashMap representing a single block scope.
+    /// Keys are variable names.
+    scopes: Vec<HashMap<String, bool>>,
+}
 
 impl Resolver {
     pub fn resolve(&mut self, statements: &[Stmt]) -> Result<()> {
@@ -9,17 +16,17 @@ impl Resolver {
 
     fn resolve_block(&mut self, block: &Block) -> Result<()> {
         self.begin_scope();
-        self.resolve(&block.statements);
+        self.resolve(&block.statements)?;
         self.end_scope();
         Ok(())
     }
 
     fn begin_scope(&mut self) {
-        todo!()
+        self.scopes.push(HashMap::new());
     }
 
     fn end_scope(&mut self) {
-        todo!()
+        self.scopes.pop();
     }
 
     fn resolve_stmt(&mut self, statement: &Stmt) -> Result<()> {
